80114facd1ff5ad1023a9f73476f9a69c9b6730a
shihuaxie <sylvia501628@gmail.com>
2025-07-22 02:26:14 +0000

    Add print_string helper function to print a string

--- helpers/print_string.c
+++ helpers/print_string.c
@@ -1,1 +1,26 @@
 #include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_string - prints  a string
+ * @args: a list of string
+ *
+ * Return: printed string
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int i = 0;
+	int count = 0;
+
+	if (!str)
+		return (0);
+
+	while (str[i])
+	{
+		count = count + _putchar(str[i]);
+		i++;
+	}
+
+	return (count);
+}
