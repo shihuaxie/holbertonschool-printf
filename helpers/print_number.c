9f62bb54d61ce474e91a9320e43001a7fde437ad
“madisonfleming” <maddyyfleming@gmail.com>
2025-07-22 05:55:59 +0000

    print_number

--- helpers/print_number.c
+++ helpers/print_number.c
@@ -1,1 +1,29 @@
-#include "main.h"
+// #include "main.h" //
+#include <stdio.h>
+#include <limits.h>
+/**
+ * print_number - print number
+ * @args: argument to print
+ * Return: number
+ */
+int print_numbers(va_list *args)
+{
+	// int n = va_arg(args, int);//
+	int sign = 0;
+	int n;
+	int num;
+
+	if (n < 0)
+	{
+		sign = sign + putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+
+	putchar((n % 10) + '0');
+
+printf("%d\n", num);
+}
