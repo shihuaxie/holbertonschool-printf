ee15f0beba008f5cea5a72da671e38ea09cdc269
shihuaxie <sylvia501628@gmail.com>
2025-07-22 01:14:01 +0000

    Update files

---
+++ _printf.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * int _printf - Function that we custom
+ * @format: The format string
+ *
+ * Return: Number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	
+}
