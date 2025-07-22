77b0ca18b0368de060fdf45567b18e29244be9f2
“madisonfleming” <maddyyfleming@gmail.com>
2025-07-22 01:43:01 +0000

    print_char

--- helpers/print_char.c
+++ helpers/print_char.c
@@ -1,2 +1,14 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_char - print a char
+ * @args: pointer to arguments
+ * Return: printed char
+ */
+int print_char(va_list *args)
+{
+	char c = va_arg(args, int);
+
+	_putchar(c);
+return (1);
+}
--- main.h
+++ main.h
@@ -3,5 +3,6 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_char(va_list *args);
 
 #endif
