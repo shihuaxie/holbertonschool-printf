a080852d1b23d7e4af4b13c178ae74fe0caf7f35
shihuaxie <sylvia501628@gmail.com>
2025-07-23 04:37:56 +0000

    Add some comments to printf file

--- _printf.c
+++ _printf.c
@@ -3,13 +3,37 @@
 #include <unistd.h>
 
 /**
- * int _printf - Function that we custom
- * @format: The format string
+ * int _printf - Function produces output according to a format
+ * supporting format specifiers: %c, %s, %d, %i, and %%.
  *
- * Return: Number of characters printed
+ * @format: The format string containing the characters
+ * and the optional format specifiers.
+ *
+ * Return: Number of characters printed, (excluding the null byte)
+ * or -1 if an error occurs
+ *
  */
 
 int _printf(const char *format, ...)
 {
+	/* access the variable arguments */
+	va_list args;
+	int i = 0;
+	int count = 0;
+
+	/* Return -1 if format string is NULL */	
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	/* while Loop through each character in the format string */
+	while (format[i])
+	{
 	
+	
+	
+	
+	}
+
 }
