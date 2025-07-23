b4e99d40a456ab06a74c4a6157f50a299259bf9b
shihuaxie <sylvia501628@gmail.com>
2025-07-23 09:48:33 +0000

    Add _printf

--- _printf.c
+++ _printf.c
@@ -1,39 +1,54 @@
 #include "main.h"
 #include <stdarg.h>
-#include <unistd.h>
+#include <stdlib.h>
 
 /**
- * int _printf - Function produces output according to a format
- * supporting format specifiers: %c, %s, %d, %i, and %%.
- *
+ *  _printf - Function produces output according to a format
  * @format: The format string containing the characters
- * and the optional format specifiers.
- *
  * Return: Number of characters printed, (excluding the null byte)
- * or -1 if an error occurs
- *
  */
-
 int _printf(const char *format, ...)
 {
-	/* access the variable arguments */
-	va_list args;
-	int i = 0;
-	int count = 0;
+        va_list args;   /* access the variable arguments */
+        int i = 0;
+        int count = 0;
 
-	/* Return -1 if format string is NULL */	
-	if (format == NULL)
-		return (-1);
+        if (format == NULL)
+                return (-1);
 
-	va_start(args, format);
+        va_start(args, format);
 
-	/* while Loop through each character in the format string */
-	while (format[i])
-	{
-	
-	
-	
-	
-	}
+        while (format[i])
+        {
+                if (format[i] == '%')
+                {
+                        i++; /* move to the next charactor*/
+                        if (!format[i])
+                                return (-1);
 
+                        /* find out which spicifer is matched */
+                        if (format[i] == 'c')
+                                count =  count + print_char(&args); /* print a charactor */
+                        else if (format[i] == 's')
+                                count = count  + print_string(&args); /* print a string */
+                        else if (format[i] == 'd' || format[i] == 'i')
+                                count = count + print_numbers(&args); /* print an integer */
+                        else if (format[i] == '%')
+                                count = count + print_percent(&args); /*print a % symbol */
+                        else
+                        {
+                        /* Unknown specifier: print as literal characters */
+                                count += _putchar('%');
+                                count += _putchar(format[i]);
+                        }
+                }
+                else
+                {
+                        /* Normal character: print it directly */
+                        count += _putchar(format[i]);
+                }
+                i++; /* Move to the next character in the format string */
+        }
+        va_end(args);
+        return (count);
 }
