1dde0b01adad3e8632c4817c7036fe064277db5b
madisonfleming <maddyyfleming@gmail.com>
2025-07-24 01:38:35 +0000

    bettycheck

--- _printf.c
+++ _printf.c
@@ -9,46 +9,43 @@
  */
 int _printf(const char *format, ...)
 {
-        va_list args;   /* access the variable arguments */
-        int i = 0;
-        int count = 0;
+	va_list args;   /* access the variable arguments */
+	int i = 0;
+	int count = 0;
 
-        if (format == NULL)
-                return (-1);
+	if (format == NULL)
+		return (-1);
 
-        va_start(args, format);
+	va_start(args, format);
 
-        while (format[i])
-        {
-                if (format[i] == '%')
-                {
-                        i++; /* move to the next charactor*/
-                        if (!format[i])
-                                return (-1);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++; /* move to the next charactor*/
+			if (!format[i])
+				return (-1);
 
-                        /* find out which spicifer is matched */
-                        if (format[i] == 'c')
-                                count =  count + print_char(&args); /* print a charactor */
-                        else if (format[i] == 's')
-                                count = count  + print_string(&args); /* print a string */
-                        else if (format[i] == 'd' || format[i] == 'i')
-                                count = count + print_numbers(&args); /* print an integer */
-                        else if (format[i] == '%')
-                                count = count + print_percent(&args); /*print a % symbol */
-                        else
-                        {
-                        /* Unknown specifier: print as literal characters */
-                                count += _putchar('%');
-                                count += _putchar(format[i]);
-                        }
-                }
-                else
-                {
-                        /* Normal character: print it directly */
-                        count += _putchar(format[i]);
-                }
-                i++; /* Move to the next character in the format string */
-        }
-        va_end(args);
-        return (count);
+			if (format[i] == 'c')
+			count =  count + print_char(&args); /* print a charactor */
+			else if (format[i] == 's')
+				count = count  + print_string(&args); /* print a string */
+			else if (format[i] == 'd' || format[i] == 'i')
+				count = count + print_numbers(&args); /* print an integer */
+			else if (format[i] == '%')
+				count = count + print_percent(&args); /*print a % symbol */
+			else
+			{
+				count += _putchar('%');
+				count += _putchar(format[i]);
+			}
+		}
+		else
+		{
+			count += _putchar(format[i]);
+		}
+		i++; /* Move to the next character in the format string */
+	}
+	va_end(args);
+	return (count);
 }
